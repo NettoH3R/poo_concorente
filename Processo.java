b9a45ac707ceb379027f7805bdb2da4367d37146
João Netto <jovinettonetto@gmail.com>
2023-10-31 15:03:48 +0000

    save

---
+++ Processo.java
@@ -0,0 +1,44 @@
+public class Processo extends Thread {
+    private String nome;
+    private int numeroInteracoes;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroInteracoes() {
+        return numeroInteracoes;
+    }
+    public void setNumeroInteracoes(int numeroInteracoes) {
+        this.numeroInteracoes = numeroInteracoes;
+
+    }
+
+    public Processo(){
+
+    }
+
+    public Processo(String nome, int numeroInteracoes, int prioridade){
+        this.nome=nome;
+        this.numeroInteracoes=numeroInteracoes;
+        setPriority(prioridade);
+    }
+
+    public void run(){
+
+        for (int i = 0; i < numeroInteracoes; i++) {
+            System.out.println("Processo: ("+this.nome+ "):" +i);
+            //yield();
+
+            try{
+                Thread.sleep(i *100);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            
+        }
+    }
+}
